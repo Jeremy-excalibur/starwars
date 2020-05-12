ef5240c10a12767e7ea4c62883f937d8f534841e
Jimd3v <51441791+Jeremy-excalibur@users.noreply.github.com>
2020-05-12 09:25:18 +0200

    Générateur 6 films

--- index.php
+++ index.php
@@ -6,23 +6,25 @@
     <title>Générateur Star Wars</title>
     <link rel="stylesheet" href="style.css">
     
-</head>
-<body>
-
+    
 <header>
 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png" alt="">
 </header>
 
 <div class="identity">
-<h1 class="titre">Générateur de personnage</h1>
+<h1 class="title">Movie Generator</h1>
+<p class="regle">Vous n'arrivez jamais à vous décider sur le film Star Wars à regarder ce soir ? Ce générateur est la pour vous aider</p>
 <br>
-    <h1 id="nom">Nom du perso</h1>
-    <p id="taille">Taille du personnage</p>
-    <p id="poid">Poid du personnage</p>
-    <p id="sexe">Sexe du personnage</p>
-    <p id="couleur-cheveux">Couleur de cheveux du personnage</p>
-
-    <button id="tableau">Générer</button>  
+<div id="bordure">
+    <h1 id="nom"></h1>
+    <img id="image" src="https://i.skyrock.net/6312/41716312/pics/1972337067_small_3.jpg" alt="">
+    <p id="episode"></p>
+    <p id="realisateur"></p>
+    <p id="resume"></p>
+    <p id="dateDeSortie"></p>
+</div>
+    <button id="tableau">Que la force soit avec toi</button>
+      
 </div>
 
 <footer>
@@ -30,7 +32,10 @@
 </footer>
     
 
-
+    
     <script src="script.js"></script>
+    
+
+    
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Appel de mes variables
 let nom = document.getElementById('nom');
-let taille = document.getElementById('taille');
-let poid = document.getElementById('poid');
-let sexe = document.getElementById('sexe');
-let couleurCheveux = document.getElementById('couleur-cheveux');
+let episode = document.getElementById('episode');
+let realisateur = document.getElementById('realisateur');
+let resume = document.getElementById('resume');
+let dateDeSortie = document.getElementById('dateDeSortie');
+let image = document.getElementById('image')
 let tableau = document.getElementById('tableau');
+let url = "https://swapi.dev/api/films/";
+
 
 
 
 // Ecouteurs
 tableau.addEventListener('click', updatePage);
 
-
-
 //Promesses
-fetch("https://swapi.dev/api/people/").then((data)=> {
+fetch(url).then((data)=> {
     data.json().then((data) => {
         tableau = data;
         console.log(tableau.results);
@@ -22,22 +23,49 @@ fetch("https://swapi.dev/api/people/").then((data)=> {
 })
 
 
-
 function updatePage(){
 
     function random(min, max)
     {
-        return Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+        return Math.floor(Math.random() * (5 - 0 + 1)) + 0;
     }
 
-    let id = random(0,10);
+    let id = random(0,5);
+
+    function imgSelect(){
+        if (tableau.results[id]["episode_id"] == "1") {
+            image.setAttribute('src', "https://vignette.wikia.nocookie.net/fr.starwars/images/e/e0/Lundi.png/revision/latest?cb=20151011153017")
+        }
+        else if (tableau.results[id]["episode_id"] == "2") {
+            image.setAttribute('src', "https://fr.web.img5.acsta.net/medias/nmedia/00/02/34/81/affclones.jpg")
+        }
+        else if (tableau.results[id]["episode_id"] == "3") {
+            image.setAttribute('src', "https://fr.web.img3.acsta.net/medias/nmedia/18/35/53/23/18423997.jpg")
+        }
+        else if (tableau.results[id]["episode_id"] == "4") {
+            image.setAttribute('src', "https://images-na.ssl-images-amazon.com/images/I/91H91NbI2xL._AC_SL1500_.jpg")
+        }
+        else if (tableau.results[id]["episode_id"] == "5") {
+            image.setAttribute('src', "https://m.media-amazon.com/images/I/51nwJJf3IjL.jpg")
+        }
+        else if (tableau.results[id]["episode_id"] == "6"){
+            image.setAttribute('src', "https://i.pinimg.com/originals/e6/91/d8/e691d863de57aed3067cae106d202174.jpg")
+        }
+        else 
+        {}
+    }
 
 
-    nom.innerHTML = tableau.results[id]["name"];
-    taille.innerText = tableau.results[id]["height"] + " cm";
-    poid.innerText = tableau.results[id]["mass"] + " kg";
-    sexe.innerText = tableau.results[id]["gender"];
-    couleurCheveux.innerHTML = tableau.results[id]["hair_color"];
+    nom.innerHTML = tableau.results[id]["title"];
+    imgSelect(id);
+    episode.innerText = "Episode n°" + tableau.results[id]["episode_id"];
+    realisateur.innerText = "Réalisateur : " + tableau.results[id]["director"];
+    resume.innerText = tableau.results[id]["opening_crawl"];
+    dateDeSortie.innerHTML = "Date de sortie : " + tableau.results[id]["release_date"];
+    
+
+
 }
+
 
 
